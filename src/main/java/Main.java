bf1c8e63b391f936bc83edb3647c9ee28bf30662
Caceresenzo <caceresenzo1502@gmail.com>
2023-12-25 18:26:08 +0100

    pass 1st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,24 @@
-import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    // try(DatagramSocket serverSocket = new DatagramSocket(2053)) {
-    //   while(true) {
-    //     final byte[] buf = new byte[512];
-    //     final DatagramPacket packet = new DatagramPacket(buf, buf.length);
-    //     serverSocket.receive(packet);
-    //     System.out.println("Received data");
-    //
-    //     final byte[] bufResponse = new byte[512];
-    //     final DatagramPacket packetResponse = new DatagramPacket(bufResponse, bufResponse.length, packet.getSocketAddress());
-    //     serverSocket.send(packetResponse);
-    //   }
-    // } catch (IOException e) {
-    //     System.out.println("IOException: " + e.getMessage());
-    // }
-  }
+	public static void main(String[] args) throws Exception {
+		System.out.println("codecrafters build-your-own-dns");
+
+		try (final var serverSocket = new DatagramSocket(2053)) {
+			while (true) {
+				final var requestBuffer = new byte[512];
+				final var request = new DatagramPacket(requestBuffer, requestBuffer.length);
+				serverSocket.receive(request);
+
+				System.out.println("Received data");
+
+				final byte[] responseBuffer = new byte[512];
+				final var response = new DatagramPacket(responseBuffer, responseBuffer.length, request.getSocketAddress());
+				serverSocket.send(response);
+			}
+		}
+	}
+
 }
